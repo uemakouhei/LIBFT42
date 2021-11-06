3d68d27535508201bdf069de8d185b07dd773582
Kouhei Uema <kuema@c1r13s2.42tokyo.jp>
2021-11-07 01:01:28 +0900

    callochayahisan

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,38 +6,18 @@
 /*   By: kuema </var/mail/kuema>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 07:05:59 by kuema             #+#    #+#             */
-/*   Updated: 2021/11/06 21:35:39 by kuema            ###   ########.fr       */
+/*   Updated: 2021/11/07 00:58:16 by kuema            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
-int	overcheck(size_t count, size_t size)
-{
-	int		countbinsize;
-
-	countbinsize = 0;
-	while (count > 0)
-	{
-		count = count / 2;
-		countbinsize++;
-	}
-	countbinsize--;
-	size--;
-	while (size)
-	{
-		if (countbinsize > 63)
-			return (0);
-		size--;
-		countbinsize++;
-	}
-	return (1);
-}
-
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*pt;
+	size_t allocate;
 
+	allocate = count * size;
 	if (count == 0 || size == 0)
 	{
 		pt = malloc(1);
@@ -48,13 +28,13 @@ void	*ft_calloc(size_t count, size_t size)
 		ft_bzero(pt, 1);
 		return (pt);
 	}
-	if (!(overcheck(size, count)))
+	if (allocate / count < size)
 		return (NULL);
-	pt = malloc(count * size);
+	pt = malloc(allocate);
 	if (pt == NULL)
 	{
 		return (NULL);
 	}
-	ft_bzero(pt, count * size);
+	ft_bzero(pt, allocate);
 	return (pt);
 }
